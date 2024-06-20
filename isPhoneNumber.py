3c6fdd5df43e4b42895bfd5eb3a6447588344c9e
Agrika <109896751+agrikatheprogrammer@users.noreply.github.com>
2024-06-20 12:41:48 -0700

    Update isPhoneNumber.py

--- isPhoneNumber.py
+++ isPhoneNumber.py
@@ -1,10 +1,11 @@
 import re
-class isPhoneNumber:
-  def isPhoneNumber(text):
-    phone_pattern = r'[0-9]{3}-\d\d\d-[0-9]{4}'
+
+def isPhoneNumber(text):
+    phone_pattern = r'[0-9]{3}-\d{3}-\d{4}'
     return bool(re.search(phone_pattern, text))
 
-if name=="__main__":
-  print(str(isPhoneNumber("123-456-7890"))) #TRUE
-  print(str(isPhoneNumber("This has a number 450-213-4221"))) #TRUE 
-  print(str(isPhoneNumber("123-421-542a"))) #FALSE
+if __name__ == "__main__":
+    print(str(isPhoneNumber("123-456-7890")))  # TRUE
+    print(str(isPhoneNumber("This has a number 450-213-4221")))  # TRUE 
+    print(str(isPhoneNumber("123-421-542a")))  # FALSE
+
